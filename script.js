c77cf664d3934d9380c9ae5ec3ef552ecee85d8b
João Paulo Almeida <joaopaulito90@gmail.com>
2026-04-23 23:44:07 -0300

    initial commit: starting my project with array lists

---
+++ script.js
@@ -0,0 +1,46 @@
+//Banco de dados feito de forma manual, ainda não aprendi sobre banco de dados de maneira "formal"
+let bibliotecaNovels = [
+  {
+    titulo: "Lord of the Mysteries",
+    genero: "Fantasia/Misterios/Steampunk/Sobrenatural",
+    notas: 9.89,
+    autor: "Cuttlefish That Loves Diving",
+    volumes: 8,
+    status: "Concluido",
+  },
+
+  {
+    titulo: "Classroom of the Elite",
+    genero: "Psicologico/Escolar/Drama",
+    notas: 9,
+    autor: "Syougo Kinugasa",
+    volumes: 14,
+    status: "Em andamento",
+  },
+  {
+    titulo: "That Time I Got Reincarnated as a Slime",
+    genero: "Fantasia/Isekai/Aventura",
+    notas: 8,
+    autor: "Fuse",
+    volumes: 20,
+    status: "Em andamento",
+  },
+  {
+    titulo: "Alya Sometimes Hides Her Feelings in Russian",
+    genero: "Comedia Romantica/Escolar",
+    notas: 7.2,
+    autor: "Sun Sun Sun",
+    volumes: 9,
+    status: "Em andamento",
+  },
+  {
+    titulo: "The Apothecary Diaries",
+    genero: "Misterio/Historico/Drama",
+    notas: 9,
+    autor: "Natsu Hyuuga",
+    volumes: 16,
+    status: "Em andamento",
+  },
+];
+
+console.log(bibliotecaNovels);
