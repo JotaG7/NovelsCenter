770205fe55cd9e14fef0f12eb78a45a9bcfad653
João Paulo Almeida Aureliano <joaopaulito90@gmail.com>
2026-04-29 23:03:29 -0300

    feat: implement filter function to find novels with 'Em andamento' status

--- script.js
+++ script.js
@@ -64,7 +64,7 @@ function sortNovels() {
   // Aqui retornamos a variavel nota depois de fazermos o calculo
 }
 
-console.log(sortNovels());
+//console.log(sortNovels());
 // Exibimos os dados no console executando a função
 
 // HASH TABLE - VERSÃO UTILIZANDO UMA FUNÇÃO DE FORMA MAIS SIMPLIFICADA
@@ -85,84 +85,17 @@ let hashNovels = hashTable(bibliotecaNovels);
 //Retorno de pesquisa para confirmar se funciona
 //console.log(hashNovels["Lord Xue Ying"].autor);
 
-/* 
+// Function utilizando o metodo filter()
 
-HASH TABLE - VERSÃO SEM UTILIZAR UMA FUNÇÃO
-
-// Variavel para fazer o hash table através da propriedade reduce()
-let hashPorTitulo = bibliotecaNovels.reduce((hashPorTitulo, tituloAtual) => {
-  //hashPorTitulo = acumulador - tituloAtual = valor atual (onde será percorrido o elemento chave do objeto)
-  console.log(tituloAtual.titulo);
-  // console.log para fins de teste e exibição
-  hashPorTitulo[tituloAtual.titulo] = hashPorTitulo[tituloAtual.titulo];
-  // Aqui basicamente como estamos fazendo uma busca de 1 para 1 ou seja, pesquisar um titulo especifico vai retornar apenas ou o titulo em especifico ou vai retornar "nada" pois o titulo não existe,
-  // mas se fosse em um retorno de varios resultados com base em uma pesquisa como genero onde tem em varios objetos dai pode-se colocar no comando acima esse "|| []" onde ele ira pegar um array vazio e retornar os resultados e no comando abaixo
-  //colocar o comando push para pegar todos os elementos que possuem e retornar eles. Como eu disse se fosse uma pequisa por genero esse metodo citado seria o ideal, mas como é por titulo esse que estou usando funciona
-  hashPorTitulo[tituloAtual.titulo] = tituloAtual;
-
-  return hashPorTitulo;
-  // depois de tudo retorna o hashPorTitulo já como objeto de hash table através do elemento "titulo" como chave para exibição de todos os elementos daquele objeto
-}, {}); // Valor inicial que é aqui onde vai sair o hashPorTitulo
-
-//Exemplo exibido no console.log
-console.log(hashPorTitulo[`Lord of the Mysteries`]);
-
-*/
-
-/*
-TABELA HASH - estou aprendendo sobre - é bom para fazer pesquisa rapida, a impossibilidade de existir elementos com nomes duplicados,
-é bom de caching/memorização dos dados não precisando solicitar do servidor toda hora e é bom em modelar relação entre dois elementos
-
-
-// Isso seria uma tabela Hash onde teoricamente ficaria mais rapido achar elementos dentro dela, pois como o elemento esta associado a uma string a resposta seria instatanea
-let novelsHash = {
-  "Lord of the Mysteries": {
-    genero: "Fantasia/Misterios/Steampunk/Sobrenatural",
-    notas: 9.89,
-    autor: "Cuttlefish That Loves Diving",
-    volumes: 8,
-    status: "Concluido",
-  },
-  "Classroom of the Elite": {
-    genero: "Psicologico/Escolar/Drama",
-    notas: 9,
-    autor: "Syougo Kinugasa",
-    volumes: 14,
-    status: "Em andamento",
-  },
-  "That Time I Got Reincarnated as a Slime": {
-    genero: "Fantasia/Isekai/Aventura",
-    notas: 8,
-    autor: "Fuse",
-    volumes: 20,
-    status: "Em andamento",
-  },
-  "Alya Sometimes Hides Her Feelings in Russian": {
-    genero: "Comedia Romantica/Escolar",
-    notas: 7.2,
-    autor: "Sun Sun Sun",
-    volumes: 9,
-    status: "Em andamento",
-  },
-  "The Apothecary Diaries": {
-    genero: "Misterio/Historico/Drama",
-    notas: 9,
-    autor: "Natsu Hyuuga",
-    volumes: 16,
-    status: "Em andamento",
-  },
-};
-
-// Função para buscar se existe ou não uma novel nessa tabela hash através do nome, se achar retorna algumas informações sobre a novel
-function buscar(nomeDaNovel) {
-  let novelEncontrada = novelsHash[nomeDaNovel];
-  if (novelEncontrada) {
-    return `O nome de sua novel é ${nomeDaNovel}, seu autor é ${novelEncontrada.autor} e seu status atual é ${novelEncontrada.status}`;
-  } else {
-    return `Novel não encontrada no catálogo`;
-  }
+function filtrarNovelsEmAndamento() {
+  let statusNovel = bibliotecaNovels.filter(
+    // Crio uma variavel e defino ela sendo o resultado do metodo filter() para o array de objetos
+    state => state.status === "Em andamento",
+    // Coloquei para fazer aplicação de filtro em um caso especifico nos status da novel que é "Em andamento", assim todas os objetos que tiverem esse status
+    // irão etornar aqui com suas respectivas informações
+  );
+  return statusNovel; // retorno do variavel
 }
 
-console.log(novelsHash["Classroom of the Elite"].notas);
-
-*/
+//exibição atraves do console.log()
+console.log(filtrarNovelsEmAndamento());
