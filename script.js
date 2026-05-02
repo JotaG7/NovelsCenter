2c4a307c49607acd9c447b55922422e53de8b5f1
João Paulo Almeida Aureliano <joaopaulito90@gmail.com>
2026-05-02 00:39:59 -0300

    feat: implement case-insensitive search and data normalization

--- script.js
+++ script.js
@@ -58,6 +58,30 @@ let bibliotecaNovels = [
     volumes: 20,
     status: "Em andamento",
   },
+  {
+    titulo: "Omniscient Reader's Viewpoint",
+    genero: "Ação/Sobrenatural/Drama",
+    notas: 9.9,
+    autor: "Sing Shong",
+    volumes: 5,
+    status: "Concluido",
+  },
+  {
+    titulo: "The Beginning After the End",
+    genero: "Fantasia/Isekai/Ação",
+    notas: 8.5,
+    autor: "TurtleMe",
+    volumes: 10,
+    status: "Em andamento",
+  },
+  {
+    titulo: "Solo Leveling",
+    genero: "Ação/Fantasia",
+    notas: 9.5,
+    autor: "Chugong",
+    volumes: 14,
+    status: "Concluido",
+  },
 ];
 
 // Aprendendo e criando uma function que utiliza o metodo sort() e tambem o Spread Operator para imutabilidade
@@ -79,38 +103,56 @@ function sortNovels() {
 function hashTable(listaDeNovels) {
   return listaDeNovels.reduce((hashPorTitulo, novelAtual) => {
     //Aqui temos ja o parametro listaDeNovels com o metodo reduce(), o acumulador é o hashPorTitulo e o valor atual é a novelAtual (onde vai passar cada objeto)
-    hashPorTitulo[novelAtual.titulo] = novelAtual;
-    // A chave para o acumulador vai ser o titulo de novelAtual ou seja, a pesquisa vai ser realizada utilizando ele e retornando o objeto completo
+    const novelTitulo = novelAtual.titulo.toLocaleLowerCase();
+    //Criei uma variavel novelTitulo onde ela recebe o elemento titulo do valor atual de reduce e o converte no diminutivo
+    hashPorTitulo[novelTitulo] = novelAtual;
+    // A chave para o acumulador vai ser a variavel novelTitulo de novelAtual que esta pegando o elemento e convertendo ele no diminutivo, ou seja,
+    // a pesquisa vai ser realizada utilizando ele e retornando o objeto completo
 
     return hashPorTitulo;
-    //Acontece o retorno do acumalador em um objeto novo
+    //Acontece o retorno do acumulador em um objeto novo
   }, {}); //Valor inicial onde vai ser o retorno de hashPorTitulo
 }
 
 //Variavel que define a função e o array de objetos como parametro
 let hashNovels = hashTable(bibliotecaNovels);
 
+//Função de busca
+//Depois de ter o diminutivo na execução do reduce() ele vai ter tbm um diminutivo na função de busca para achar sempre a versão diminutiva que esta na função hash
+function buscarNovel(buscaTitulo) {
+  return hashNovels[buscaTitulo.toLocaleLowerCase()];
+  //Pegamos a variavel hashNovels e fazemos o processo de busca no diminutivo que esta salvo na tabela hash
+}
+
 //Retorno de pesquisa para confirmar se funciona
-//console.log(hashNovels["Lord Xue Ying"].autor);
+console.log(buscarNovel("Lord"));
 
 // Function utilizando o metodo filter() mas agora utilizando a pesquisa dinamica onde eu coloco o status que eu quero da novel e ela me retorna todos os resultados
 
 function filtrarPorStatus(statusDesejado) {
   // crio uma função com o parametro "statusDesajado"
-  return bibliotecaNovels.filter(state => state.status === statusDesejado);
+  return bibliotecaNovels.filter(
+    state =>
+      state.status.toLocaleLowerCase() === statusDesejado.toLocaleLowerCase(),
+  );
   //Retorno o array ja aplicado com filter() onde o state.status é o resultado de uma pesquisa sobre o nome especifico que esta no objeto e o resultado é todos os objetos que tenham esse elemento
 }
 
 // Resultado exibido no console.log()
-//console.log(filtrarPorStatus("Em andamento"));
+//console.log(filtrarPorStatus("concluido"));
 
 // Criando uma função utilizando tudo que estava aprendendo nos ultimos commits filter(), sort() e um novo slice()
 function topNovelsPorGenero(generoDesejado) {
   return (
     bibliotecaNovels
-      .filter(gender => gender.genero?.includes(generoDesejado))
+      .filter(gender =>
+        gender.genero
+          ?.toLocaleLowerCase()
+          .includes(generoDesejado.toLocaleLowerCase()),
+      )
       //utilizo o metodo filter() para retornar novels pelo genero escolhido e acrescento o includes para verificação do genero em meio aos caracteres especiais
       // o "gender.genero?" foi basicamente adicionado para não haver quebra de codigo caso não aja a propriedade genero em algum objeto
+      //coloquei o toLowerCase() pois o includes() diferencia maisculo de minusculo e com o toLowerCase() mesmo fazendo a pesquisa colocando o nome em minusculo ele consegue fazer a busca e achar o que pede
       .sort((a, b) => b.notas - a.notas)
       // com o metodo sort() eu faço com que depois da filtragem ele ordene esse filtro da maior para a menor nota
       .slice(0, 3)
@@ -119,4 +161,4 @@ function topNovelsPorGenero(generoDesejado) {
 }
 
 //Exibição para o resultado da function topNovelsPorGenero
-console.log(topNovelsPorGenero("Fantasia"));
+console.log(topNovelsPorGenero(""));
